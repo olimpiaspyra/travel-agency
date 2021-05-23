ad9029df29a9f54c9bcc14c61d4575c2834429de
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-05-23 22:54:13 +0200

    Add views (in progress)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
@@ -10,9 +10,14 @@ import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
+import Countries from './components/views/Countries/CountriesContainer.js';
+import Regions from './components/views/Regions/RegionsContainer.js';
+import Country from './components/views/Country/CountryContainer.js';
+import Trip from './components/views/Trip/TripContainer.js';
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
+import { AnimatedSwitch } from 'react-router-transition';
 
 class App extends React.Component {
   static propTypes = {
@@ -37,13 +42,21 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
+          <AnimatedSwitch
+            atEnter={{opacity: 0, top: 200}}
+            atLeave={{opacity: 0}}
+            atActive={{opacity: 1, top: 0}}
+            location={location}>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
             {/* TODO - add more routes for other views */}
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/regions' component={Regions} />
             <Route exact path='/info' component={Info} />
+            <Route exact path="/country/:id" component={Country} />
+            <Route exact path="/trip/:id" component={Trip} />
             <Route path='*' component={NotFound} />
-          </Switch>
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
